f27abd5584798adda3f6ab3b9ae7966f36151028
jiechau_huang <jiechau_huang@friday.tw>
2023-10-05 07:07:15 +0800

    single worker mn.py

--- mn.py
+++ mn.py
@@ -1,41 +1,65 @@
-
-import os
-os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
 import tensorflow as tf
 from tensorflow import keras
+
+import json
+import os
+
+#tf_config = json.loads(os.environ['TF_CONFIG'])
+#cluster_spec = tf_config['cluster'] 
+#task_type = tf_config['task']['type']
+#task_id = tf_config['task']['index']
+#num_workers = len(tf_config['cluster']['worker'])
+#print(tf_config)
+
+
+def build_model():
+    model = keras.Sequential()
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D((2, 2)))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(64, activation='relu'))
+    model.add(keras.layers.Dense(10, activation='softmax'))
+    return model
 
 # ndarray, 60000 train, and 10000 test
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 x_train = x_train.reshape(-1, 28, 28, 1).astype("float32") / 255.0
 x_test = x_test.reshape(-1, 28, 28, 1).astype("float32") / 255.0
 # dataset
+num_workers = 1
 per_worker_batch_size = 64
-global_batch_size = per_worker_batch_size * 1
+global_batch_size = per_worker_batch_size * num_workers
 multi_worker_dataset = tf.data.Dataset.from_tensor_slices(
       (x_train, y_train)).shuffle(60000).repeat().batch(global_batch_size)
 
-# 构建模型
-model = keras.Sequential()
-model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(keras.layers.MaxPooling2D((2, 2)))
-model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(keras.layers.MaxPooling2D((2, 2)))
-model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(64, activation='relu'))
-model.add(keras.layers.Dense(10, activation='softmax'))
 
-# 编译和训练模型
+# (1) build
+model = build_model()
+# (2) load
+#model = keras.models.load_model('/tmp/my_model_mn') # all workers should use chief's version
+
+# compile
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=2, batch_size=64) # default batch_size=32
-#model.fit(multi_worker_dataset, epochs=2, steps_per_epoch=int(60000/global_batch_size))
 
-# 评估模型
+callbacks = [tf.keras.callbacks.ModelCheckpoint('/tmp/my_model_mn', save_freq='epoch')]
+
+# experimental_distribute_dataset
+#dist_dataset = strategy.experimental_distribute_dataset(multi_worker_dataset) 
+
+#model.fit(x_train, y_train, epochs=2, batch_size=64) # default batch_size=32
+#model.fit(multi_worker_dataset, epochs=1, steps_per_epoch=int(60000/global_batch_size))
+model.fit(multi_worker_dataset, epochs=10, steps_per_epoch=int(60000/global_batch_size), callbacks=callbacks)
+#model.fit(dist_dataset, epochs=10, steps_per_epoch=int(60000/global_batch_size), callbacks=callbacks)
+
+# evaluate
 loss, accuracy = model.evaluate(x_test, y_test)
 print(accuracy)
 
+# this way, every worker save model
+# but not a good practice
 model.save('/tmp/my_model_mn')
