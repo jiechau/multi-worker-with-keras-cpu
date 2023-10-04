8d9f5123772b99ca42de5498c8489800bdd5a0a4
jiechau_huang <jiechau_huang@friday.tw>
2023-10-04 20:45:05 +0800

    checkpoint_manager

--- dmn.py
+++ dmn.py
@@ -66,17 +66,6 @@ with strategy.scope():
     #model = keras.models.load_model('/tmp/my_model_mn') # all workers should use chief's version
     # (3)
     model = keras.Sequential()
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-    model.add(keras.layers.MaxPooling2D((2, 2)))
-    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(keras.layers.MaxPooling2D((2, 2)))
-    model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(64, activation='relu'))
-    model.add(keras.layers.Dense(10, activation='softmax'))
-
-
-
-
 
 
     # compile
